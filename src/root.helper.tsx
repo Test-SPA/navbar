d926c1a300df2ab79db2c6b7c39bb3a0d3e79f03
Layeeq <layqahmed@gmail.com>
2023-12-24 22:10:52 -0600

    init for nav bar

---
+++ src/root.helper.tsx
@@ -0,0 +1,14 @@
+export const links = [
+    {
+        name: "Home",
+        href: "/",
+    },
+    {
+      name: "TPeople",
+      href: "/people",
+    },
+    {
+      name: "TPlanets",
+      href: "/planets",
+    },
+  ];
